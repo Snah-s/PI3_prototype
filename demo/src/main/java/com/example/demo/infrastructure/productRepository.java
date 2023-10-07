a10995c89c8dfa41e944b09ef9d22aeccb0e3610
Snah-s <camiloernestosoto@gmail.com>
2023-10-07 17:24:08 -0500

    error in packages selections

--- demo/src/main/java/com/example/demo/infrastructure/productRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.infrastructure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domain.product;
-
-public interface productRepository extends JpaRepository<product, Long> {
-}
